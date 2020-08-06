5e5248e7a23df5c166cbc200184862b0f8f4d004
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2020-08-06 11:34:08 -0700

    create tests dir and tests for get df utils.

--- utils.py
+++ utils.py
@@ -1,19 +1,31 @@
 import os
+import features
 import pandas as pd
 LICHESS_CSV_PATH = 'csvs/lichess/{year}-{month:0>2}/'
 FEATURE_CSV_PATH = 'csvs/{feature_name}/{year}-{month:0>2}/'
+CSV_PATH = 'csvs/{type}/{year}-{month:0>2}/'
 
 
-def get_df(years=[2013], months=[1], shards=None):
-    PATH = LICHESS_CSV_PATH
-
+def get_type_df(type='lichess', years=[0], months=[0], shards=None):
     dfs = []
     for year in years:
         for month in months:
-            path = PATH.format(year=year, month=month)
+            path = CSV_PATH.format(type=type, year=year, month=month)
             if shards is None:
                 shards = range(len(os.listdir(path)))
             for shard in shards:
                 df = pd.read_csv(path + str(shard) + '.csv')
                 dfs.append(df)
     return pd.concat(dfs).reset_index(drop=True)
+
+
+def get_df(types=None, years=[0], months=[0], shards=None):
+    df = None
+    for type in types:
+        type_df = get_type_df(type, years, months, shards)
+        if df is None:
+            df = type_df
+        else:
+            assert len(df) == len(type_df) # TODO: check indices match?
+            df = df.join(type_df)
+    return df
