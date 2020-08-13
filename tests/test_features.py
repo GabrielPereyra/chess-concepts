e5edd6f331b92e6b581a52a34fca4069ff959119
pkacprzak <pawel.kacprzak.89@gmail.com>
2020-08-13 18:31:07 +0200

    Add fork motive feature

--- tests/test_features.py
+++ tests/test_features.py
@@ -118,6 +118,65 @@ def test_back_rank_mate(fen, pv, expected):
     assert f.features()["is_back_rank_mate"] == expected
 
 
+@pytest.mark.parametrize(
+    "fen, pv, expected_contains_fork, expected_is_first_move_fork",
+    [
+        # First move is a simple knight fork
+        # https://lichess.org/analysis/3k4/N3q3/8/8/8/8/8/3K4_w_-_-_0_1
+        ("3k4/N3q3/8/8/8/8/8/3K4 w - - 0 1", "['a7c6', 'd8e8', 'c6e7']", True, True,),
+        # First move is a double attack with queen on king and knight
+        # https://lichess.org/analysis/rnbqkb1r/pp2pppp/3p4/3P4/4n3/8/PP3PPP/RNBQKBNR_w_KQkq_-_0_1
+        (
+            "rnbqkb1r/pp2pppp/3p4/3P4/4n3/8/PP3PPP/RNBQKBNR w KQkq - 0 1",
+            "['d1a4', 'b8d7', 'a4e4']",
+            True,
+            True,
+        ),
+        # Our first move is not a fork, but our second move is knight fork, from FIDE WCC 2004
+        # https://lichess.org/analysis/r5k1/1p1q1pbp/6p1/2Pp4/p3nQ2/P4P2/B2B2PP/2R4K_b_-_-_0_1
+        (
+            "r5k1/1p1q1pbp/6p1/2Pp4/p3nQ2/P4P2/B2B2PP/2R4K b - - 0 1",
+            "['e4f2', 'h1g1', 'f2d3', 'f4e3', 'd3c1']",
+            True,
+            False,
+        ),
+        # Known opening fork after capture
+        # https://lichess.org/analysis/r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/2N2N2/PPPP1PPP/R1BQK2R_b_KQkq_-_0_1
+        (
+            "r1bqkb1r/pppp1ppp/2n2n2/4p3/2B1P3/2N2N2/PPPP1PPP/R1BQK2R b KQkq - 0 1",
+            "['f6e4', 'c3e4', 'd7d5', 'c4d3', 'd5e4']",
+            True,
+            False,
+        ),
+        # Starting position with some example line
+        # https://lichess.org/analysis/rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR_w_KQkq_-_0_1
+        (
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            "['d2d4', 'd7d5', 'c2c4', 'e7e6', 'b1c3']",
+            False,
+            False,
+        ),
+        # First move forks the pieces but the second move is mate instead of a capture of one of forked pieces
+        # https://lichess.org/analysis/6kq/8/8/4n3/4p3/8/5P2/4RBK1_b_-_-_0_1
+        (
+            "6kq/8/8/4n3/4p3/8/5P2/4RBK1 b - - 0 1",
+            "['e4f3', 'g1g2', 'h8h2']",
+            False,
+            False,
+        ),
+    ],
+)
+def test_forks(fen, pv, expected_contains_fork, expected_is_first_move_fork):
+    f = features.Motives(fen, pv)
+    assert f.features()["contains_fork"] == expected_contains_fork
+    assert f.features()["is_first_move_fork"] == expected_is_first_move_fork
+
+
+def test_is_first_move_fork(fen, pv, expected):
+    f = features.Motives(fen, pv)
+    assert f.features()["contains_fork"] == expected
+
+
 def test_from_df():
     df = pd.DataFrame(
         [
