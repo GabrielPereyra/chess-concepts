4b4182771e398e4d0afb0c85e84840fc5fa4406b
pkacprzak <pawel.kacprzak.89@gmail.com>
2020-09-09 00:19:37 +0200

    Add more tactic related threats

--- board/threats/__init__.py
+++ board/threats/__init__.py
@@ -3,9 +3,26 @@ from typing import Dict, Callable
 
 import chess
 
+import board
 from .capture import creates_hanging_piece_threat_capture, creates_material_gain_capture
 from .mate import creates_mate_threat
-from .fork import creates_fork_threat
+
+
+def make_tactic_threat_detector(tactic: board.Tactic):
+    def detector(fen: str, move: chess.Move):
+        aug = board.AugBoard(fen)
+
+        # TODO: do we want to exclude checks?
+        if aug.gives_check(move):
+            return False
+
+        aug.push(move)
+        aug.push(chess.Move.null())
+        return any(
+            tactic in aug.move_tactics(next_move) for next_move in aug.legal_moves
+        )
+
+    return detector
 
 
 # TODO: consider refactoring to enum.IntFlag if we want them to behave like bit flags
@@ -15,7 +32,14 @@ class Threat(IntEnum):
     MATE = 1
     HANGING_PIECE_CAPTURE = 2
     MATERIAL_GAIN_CAPTURE = 3
-    FORK = 4
+    DISCOVERED_ATTACK = 4
+    FORK = 5
+    SKEWER = 6
+    # TODO: PIN should rather not be there - is it a valid assumption?
+    # TODO: For SACRIFICE on one hand it makes sense (e.g. threat to make greek gift) but on the other it does not
+    #  because for us sacrifice only makes sense when it is a good move as otherwise moving a piece so that it
+    #  is hanging will also be classified as a sacrifice which is fine but having a threat of doing this
+    #  does not make much sense
 
     @classmethod
     def detectors(cls) -> Dict["Threat", Callable[[str, chess.Move], bool]]:
@@ -23,7 +47,11 @@ class Threat(IntEnum):
             cls.MATE: creates_mate_threat,
             cls.HANGING_PIECE_CAPTURE: creates_hanging_piece_threat_capture,
             cls.MATERIAL_GAIN_CAPTURE: creates_material_gain_capture,
-            cls.FORK: creates_fork_threat,
+            cls.FORK: make_tactic_threat_detector(board.Tactic.FORK),
+            cls.DISCOVERED_ATTACK: make_tactic_threat_detector(
+                board.Tactic.DISCOVERED_ATTACK
+            ),
+            cls.SKEWER: make_tactic_threat_detector(board.Tactic.SKEWER),
         }
 
     @classmethod
